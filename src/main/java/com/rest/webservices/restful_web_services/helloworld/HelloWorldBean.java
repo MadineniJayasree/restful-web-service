b8c19eb8320916dc7e8a9a661fb1d9b9f4e70972
Madineni Jayasree <jayasreemadineni20@gmail.com>
2025-10-04 21:28:43 +0530

    HelloWorldBean addition

---
+++ src/main/java/com/rest/webservices/restful_web_services/helloworld/HelloWorldBean.java
@@ -0,0 +1,14 @@
+package com.rest.webservices.restful_web_services.helloworld;
+
+public class HelloWorldBean {
+    private String message;
+    public HelloWorldBean(String message){
+        this.message = message;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+    public String getMessage(){
+        return message;
+    }
+}
